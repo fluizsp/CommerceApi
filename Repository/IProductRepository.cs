dbe93dcc367c87e0452f883840d23d69a23f7b0e
Fábio Luiz <fabio_lsp@msn.com>
2024-02-05 11:16:57 -0300

    Fixing interfaces.

--- Repository/IProductRepository.cs
+++ Repository/IProductRepository.cs
@@ -2,12 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using CommerceApi.Models;
 
-public interface IProductRepository
+public interface IProductRepository : IRepository<Product>
 {
-    IEnumerable<Product> GetAll();
-    Product GetById(int id);
-    void Add(Product entity);
-    void Update(Product entity);
-    void Delete(Product entity);
     IEnumerable<Product> GetByPriceRange(int priceMin, int priceMax);
 }
